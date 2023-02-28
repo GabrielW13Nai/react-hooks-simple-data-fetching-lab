cae9784c6c9d36918d1dcf61fd4f66ae4b808e7b
Gabriel <gabriel.wambugu@student.moringaschool.com>
2023-02-28 22:58:44 +0300

    Lab done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import React, { useEffect, useState } from "react"
+import { data } from "../mocks/data";
+
+
+function App(){
+const [dog, setDog] = useState(data.message);
+
+
+  useEffect(()=>{
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((r)=> r.json())
+    .then((info)=> setDog(info.message));
+},[])
+
+
+    return(<>
+       <p>Loading...</p>
+       <img
+       src={dog} 
+       alt="A Random Dog"/>
+      </>
+) 
+}   
+
+export default App;
